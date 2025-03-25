50164d8a9e583b6c8175e838239f31ce28cb5b6f
fr2525 <fr2525@gmail.com>
2025-03-25 16:14:25 -0300

    Aula 12 - Criação da tela de listagem de usuarios

--- application/views/usuarios/index.php
+++ application/views/usuarios/index.php
@@ -1,6 +1,8 @@
 
 <?php $this->load->view('layout/sidebar'); ?>
 
+
+
 <!-- Main Content -->
 <div id="content">
 
@@ -9,8 +11,8 @@
   <!-- Begin Page Content -->
   <div class="container-fluid">
 
-        <!-- Page Heading -->
-        <h1 class="h3 mb-2 text-gray-800">Tables</h1>
+          <!-- Page Heading -->
+          <h1 class="h3 mb-2 text-gray-800">Tables</h1>
           <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
 
           <!-- DataTales Example -->
@@ -27,23 +29,31 @@
                       <th>Usuario</th>
                       <th>Login</th>
                       <th>Ativo</th>
-                      <th>Ações</th>
+                      <th>Acoes</th>
                     </tr>
                   </thead>
                   <tbody>
-                    <?php foreach ($usuarios as user) { ?>
+                    <?php foreach ($usuarios as $usuario) :?>
                     <tr>
-                      <td><?php $user->id ?></td>
-                      <td><?php $user->username ?></td>
+                      <td><?php echo $usuario->id ?></td>
+                      <td><?php echo $usuario->username ?></td>
+                      <td><?php echo $usuario->email ?></td>
+                      <td><?php echo $usuario->active ?></td>
+                      <td>
+                        <a title="Editar" href="" class="btn btn-primary">Editar</a>
+                        <a title="Excluir"  href="" class="btn btn-danger">Excluir</a>
+                      </td> 
                     </tr>
-                    <?php }; ?>
+                    <?php endforeach; ?>
                   </tbody>
                 </table>
               </div>
             </div>
           </div>
 
-        </div>
+
+
+  </div>
   <!-- /.container-fluid -->
 
 </div>
